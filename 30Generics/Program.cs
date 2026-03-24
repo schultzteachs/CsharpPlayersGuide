70c70f8d8cad856cac41de99192ffd87a01e19d9
Schultz <craig.schultzii@gcpsk12.org>
2026-03-24 18:37:29 -0400

    Started chapter 30 but it makes no sense yet

---
+++ 30Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿//not done
+
+List<object> ColoredItems = new List<object>();
+
+
+
+public class Sword { }
+public class Bow { }
+public class Axe{ }
+
+
+public class ColoredItemList<T> {
+
+   private T[]= new T;
+
+   public void Add(T newValue)
+    {
+
+    }
+
+}
