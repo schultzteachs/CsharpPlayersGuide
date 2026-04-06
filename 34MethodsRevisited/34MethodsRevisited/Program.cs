e94516910146d5784ca8b12cb2b48eb2488d113b
Schultz <craig.schultzii@gcpsk12.org>
2026-04-06 17:12:43 -0400

    started chap 34

---
+++ 34MethodsRevisited/34MethodsRevisited/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Random _random = new Random();
+
+int RollDie(int sides = 6) => _random.Next(sides) + 1;
+
+Console.WriteLine("Choose a number of sides for the die...");
+int Num = int.Parse(Console.ReadLine());
+
+RollDie(Num);
