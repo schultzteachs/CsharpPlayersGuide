cfcf2d5195efc0fd8f55f24d87db2b4a0550c09a
SSSchultz <craigrobertschultz.com>
2026-04-24 07:52:03 -0400

    sync of repo from computer a

--- 34MethodsRevisited/34MethodsRevisited/Program.cs
+++ 34MethodsRevisited/34MethodsRevisited/Program.cs
@@ -1,25 +1,57 @@
 ﻿
-Console.WriteLine("Give a number:");
+//34 Challenge - Better Random
+//Write an extension for Random.NextDouble() to return a random number between 0 and another number
 
-while (true)
+
+
+//Method for choosing between random strings: "up", "down", "left", "right"
+
+
+
+
+//Coin toss sim method that returns bool with optional changes in probabilities 
+
+
+
+
+
+
+
+
+
+
+
+
+public class Challenge1
 {
-    string s = Console.ReadLine();
 
-    if (int.TryParse(s, out int result))
+
+    public void NumberParser()
     {
-        Console.WriteLine("That is a number!");
-        break;
-    }
-    else if (double.TryParse(s, out double Dresult))
-    {
-        Console.WriteLine("That is a decimal, try again!");
-    }
-    else if (bool.TryParse(s, out bool Bresult))
-    {
-        Console.WriteLine("That is a boolean, try again!");
-    }
-    else
-    {
-        Console.WriteLine("That is not a number, try again!");
+        Console.WriteLine("Give a number:");
+
+        while (true)
+        {
+            string s = Console.ReadLine();
+
+            if (int.TryParse(s, out int result))
+            {
+                Console.WriteLine("That is a number!");
+                break;
+            }
+            else if (double.TryParse(s, out double Dresult))
+            {
+                Console.WriteLine("That is a decimal, try again!");
+            }
+            else if (bool.TryParse(s, out bool Bresult))
+            {
+                Console.WriteLine("That is a boolean, try again!");
+            }
+            else
+            {
+                Console.WriteLine("That is not a number, try again!");
+            }
+        }
+
     }
 }
