f86626824d03fd48a3e8810422dc4692a9e36b18
Schultz <craig.schultzii@gcpsk12.org>
2026-04-29 09:55:24 -0400

    Finished chapter 35 challenge on exceptions

--- 35CookieExceptionGame/Program.cs
+++ 35CookieExceptionGame/Program.cs
@@ -4,6 +4,9 @@
 
 
 
+CookieGame start = new CookieGame();
+
+start.StartGame();
 
 
 //start a new game
@@ -15,53 +18,107 @@ public class CookieGame()
    
     public List<int> ChosenNum = new List<int>();
 
-    public List<int> NumAvail = new List<int> { 0,1,2,3,4,5,6,7,8,9};
+    public List<int> NumAvail = new List<int> {0,1,2,3,4,5,6,7,8,9};
 
-    private Random rnd = new Random();
+    public Random rnd = new Random();
 
-    private int NastyOatmealCookie = 0;
+    public int NastyOatmealCookie  = 2;
+        
+    
 
     string? response = "";
 
     int choice = 0;
 
+    private bool GameOn = false;
+
+
+ 
 
 
     public void StartGame()
     {
         NastyOatmealCookie = rnd.Next(10);
+        
         try
         {
+            
             Console.WriteLine("Who will go first? Say 1 or 2 and press enter");
             response = Console.ReadLine();
+            if (response == "1")
+            {
+                Console.WriteLine("Ok. Player One will go first!");
+                PlayerOneTurn = true;
+                GameOn = true;
+            }
+            if (response == "2")
+            {
+                Console.WriteLine("Ok. Player Two will go first!");
+                PlayerOneTurn = false;
+                GameOn = true;
+            }
         }
         catch (Exception) {
             Console.WriteLine("Something was off. Player One will go first!");
             PlayerOneTurn = true;
+            GameOn = true;
         } 
-        if (response == "1")
+
+
+        
+        while (GameOn)
         {
+            Console.WriteLine("Pick a number! 0 to 9 and one that has not been chosen...");
 
+            try
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (NumAvail.Contains(choice))
+                {
+                    NumAvail.Remove(choice);
+                    ChosenNum.Add(choice);
+                    PlayerOneTurn = !PlayerOneTurn;
+                }
+                else if (!(NumAvail.Contains(choice)))
+                {
+                    Console.WriteLine("That number has been chosen. Pick another...");
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong... let's try again...");
+                }
+
+                if (choice == NastyOatmealCookie)
+                {
+                    GameOn = false;
+                    throw new NastyException();
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Your choice needs to be a number!");
+            }
+            catch (NastyException)
+            {
+                switch (PlayerOneTurn)
+                {
+                    case true: Console.WriteLine("Player One Wins!"); Console.WriteLine("Dear Player Two,"); break;
+                    case false: Console.WriteLine("Player Two Wins!"); Console.WriteLine("Dear Player One,"); break;
+                }
+                Console.Write("You get the nasty oatmeal cookie. gg");
+            }
+            
         }
-        if (response == "2")
-        {
+        Console.ReadKey();
+    }
 
-        }
+    public class NastyException() : Exception
+    {
+        
 
-        //Turn system
-        //When a player makes a choice, check if already chosen in ChosenNumList using List.Contains
-
-        //Chosen numbers should be removed from NumAvail and placed in Chosen Num List
-
-
-       
-        //Throw exception when oatmeal is chosen
-        //At end of turn, player1 should equal player2  
     }
 
 
-
-
-    
 
 }
